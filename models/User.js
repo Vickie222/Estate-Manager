209e1827388469b1bde6aadc9062c9a71ba98a22
Vickie222 <104118961+Vickie222@users.noreply.github.com>
2024-12-18 08:40:49 +0100

    User.js

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phone: {type: Number, required: true},
+  address: {type: String, required: true},
   password: { type: String, required: true },
   role: { type: String, enum: ['Agent', 'Buyer'], default: 'Buyer' },
 }, { timestamps: true });
